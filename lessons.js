a5950d704906d5c0a3a9a2606132433755feff38
Opeyemi541 <120164089+Opeyemi541@users.noreply.github.com>
2022-12-09 11:02:11 +0400

    lessons added

---
+++ lessons.js
@@ -0,0 +1,98 @@
+let lesson = [
+    {   
+        "id":"001",
+        "Subject":"Math",
+        "Location":"London",
+        "Price":"350",
+        "Space":5,
+        "image":"Math4-web.jpg",
+        
+    },
+    {   
+        "id":"002",
+        "Subject":"English",
+        "Location":"Dubai",
+        "Price":"360",
+        "Space":5,
+        "image":"english.jpg",
+        
+
+    },
+    {
+        "id":"003",
+        "Subject":"Computer Science",
+        "Location":"Norway",
+        "Price":"250",
+        "Space":5,
+        "image":"computer science.jpg",
+        
+
+    },
+    {
+        "id":"004",
+        "Subject":"History",
+        "Location":"India",
+        "Price":"200",
+        "Space":5,
+        "image":"history.jpg",
+        
+
+    },
+    {
+        "id":"005",
+        "Subject":"Geography",
+        "Location":"Abu Dahbi",
+        "Price":"300",
+        "Space":5,
+        "image":"geography.jpg",
+        
+    },
+    {
+        "id":"006",
+        "Subject":"Social Studies",
+        "Location":"New York",
+        "Price":"220",
+        "Space":5,
+        "image":"ss.jpg",
+        
+
+    },
+    {
+        "id":"007",
+        "Subject":"Psychology",
+        "Location":"London",
+        "Price":"320",
+        "Space":5,
+        "image":"psy.jpg",
+        
+
+    },{
+        "id":"008",
+        "Subject":"Biology",
+        "Location":"Jumiera",
+        "Price":"170",
+        "Space":5,
+        "image":"bio.jpeg",
+        
+
+    },
+    {
+        "id":"009",
+        "Subject":"Nuclear Physics",
+        "Location":"China",
+        "Price":"500",
+        "Space":5,
+        "image":"NP.jpg",
+        
+    },
+    {
+        "id":"010",
+        "Subject":"Rocket Science",
+        "Location":"Japan",
+        "Price":"510",
+        "Space":5,
+        "image":"rockect sci.jpg",
+        
+
+    }
+]
